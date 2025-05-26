5c7ab30809f10e7a13d393e7f6b209bf5fcd0739
JoacoSch <joacoschvartzman@gmail.com>
2025-05-26 09:03:52 -0300

    lañeñleñeññeñ

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -6,7 +6,10 @@ public class CollisionDetector : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("contact");
-        Destroy(gameObject);
+        Debug.Log("contact with" + collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
